ea55dfce1b8174b23d4946f9b0f28dc43b51b8c6
IT-Gaigo-WebProgrammingCourse <htun@osf.ac.jp>
2023-02-14 17:08:36 +0900

    deep diving java data types

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int i = 12;
+        boolean b = false;
+        char c = 'C';
+
+//        Whole Number
+//        byte, short, int, long
+
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 1213;
+        long longVar = 123;
+
+//        Decimal Number
+//        float, double
+
+        float floatVar = 10/3f;
+        double doubleVar = 10/3f;
+
+        System.out.println(floatVar); // 6－7桁
+        System.out.println(doubleVar); // 15桁
+
+
+//        boolean
+        boolean booleanVar = false;
+
+//        char
+        char charVar = 'A';
+        char char1 = 98; // 98 is ASCII Number code
+        System.out.println(char1);
+    }
+}
